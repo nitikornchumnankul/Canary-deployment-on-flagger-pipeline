bf91cc4889011bf27ca63618ab51c3812c5964a0
Nitikorn Chumnankul <nitikornchumnankul@gmail.com>
2019-08-06 02:36:37 +0700

    ย้ายไฟล์ ref เข้า repo

---
+++ e-commerce version 0/angular-src/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import Swal from 'sweetalert2'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app works!';
+
+  
+  }
+
