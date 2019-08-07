5ce7e509dd59aa50c2f15cb9db5cec90b86b01ac
nitikornchumnankul <nitikornchumnankul@gmail.com>
2019-08-08 02:19:19 +0700

    สร้าง Entity Product และUser close#4

---
+++ e-commerce/backend/model/user.js
@@ -0,0 +1,57 @@
+// import mongoose ,bcrypejs, config database
+const mongoose = require('mongoose');
+const bcrypejs = require('bcryptjs');
+// const config   = require('') 
+
+// User Schema
+const UserSchema = mongoose.Schema({
+    name :{
+        type:String
+    },
+    email :{
+        type:String
+    },
+    username: {
+        type: String
+    },
+    password: {
+        type: String,
+        required : true
+    },
+    role :{
+        type: String,
+        required: true
+    }
+});
+
+
+
+
+
+const User = module.exports = mongoose.model('User',UserSchema);
+module.exports.getUserById = function(id,callback){
+    User.findById(id,callback);
+}
+
+module.exports.getUserByUsername = function(username,callback){
+    const query ={username:username}
+    User.findOne(query,callback);
+}
+
+module.exports.addUser = function(newUser,callback){
+    bcrypejs.getSalt(10,(err,salt) =>{
+        bcrypt.hash(newUser.password,salt,(err,hash)=>{
+            if(err) throw err;
+            newUser.password = hash;
+            newUser.save(callback);
+        });
+    });
+}
+
+module.exports.comparePassword = function(candidatePassword,hash,callback){
+    bcrypt.compare(candidatePassword,hash,(err,isMatch)=>{
+        if(err) throw err;
+
+        callback(null,isMatch)
+    })
+}
