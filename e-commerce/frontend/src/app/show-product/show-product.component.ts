56c805c8c26f7d735d16acecc60549d3a386f062
nitikornchumnankul <nitikornchumnankul@gmail.com>
2019-08-08 02:18:48 +0700

    สร้าง Entity Product และUser close#4

---
+++ e-commerce/frontend/src/app/show-product/show-product.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ICarouselConfig, AnimationConfig } from 'angular4-carousel';
+
+@Component({
+  selector: 'app-show-product',
+  templateUrl: './show-product.component.html',
+  styleUrls: ['./show-product.component.css']
+})
+export class ShowProductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
