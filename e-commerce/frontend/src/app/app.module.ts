bc994dee2410642aef42066eaaaa745de6f8f349
Nitikorn Chumnankul <nitikornchumnankul@gmail.com>
2019-08-12 03:39:48 +0700

    add-product home login navbar register component

--- e-commerce/frontend/src/app/app.module.ts
+++ e-commerce/frontend/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddProductComponent } from './components/add-product/add-product.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddProductComponent,
+    HomeComponent,
+    NavbarComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
