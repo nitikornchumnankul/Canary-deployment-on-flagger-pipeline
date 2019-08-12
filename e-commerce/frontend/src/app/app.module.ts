c96ba79d9b1efea8aa65d46bf7de92ee038e9867
Nitikorn Chumnankul <nitikornchumnankul@gmail.com>
2019-08-12 18:32:19 +0700

    Router Link Active

--- e-commerce/frontend/src/app/app.module.ts
+++ e-commerce/frontend/src/app/app.module.ts
@@ -4,15 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/register/register.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import {Routes, RouterModule} from "@angular/router";
+
+const appRoutes:Routes =[
+  { path:'',component:HomeComponent},
+  { path:'register',component:RegisterComponent}
+]
+
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
