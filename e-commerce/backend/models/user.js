b9c1cba204a2d36f66b1ccd3c9ba0a53e7971045
Nitikorn Chumnankul <nitikornchumnankul@gmail.com>
2019-08-13 04:19:51 +0700

    register ทำงานได้แล้ว controllerทำงานได้ทุกตัว close#3

--- e-commerce/backend/models/user.js
+++ e-commerce/backend/models/user.js
@@ -5,53 +5,53 @@ const config   = require('../config/database')
 
 // User Schema
 const UserSchema = mongoose.Schema({
-    name :{
-        type:String
+    name: {
+      type: String
     },
-    email :{
-        type:String
+    email: {
+      type: String,
+      required: true
     },
     username: {
-        type: String
+      type: String,
+      required: true
     },
     password: {
-        type: String,
-        required : true
+      type: String,
+      required: true
     },
-    role :{
-        type: String,
-        required: true
+    role: {
+      type:String,
+      required:true
     }
-});
-
-
-
-
-
-const User = module.exports = mongoose.model('User',UserSchema);
-module.exports.getUserById = function(id,callback){
-    User.findById(id,callback);
-}
-
-module.exports.getUserByUsername = function(username,callback){
-    const query ={username:username}
-    User.findOne(query,callback);
-}
-
-module.exports.addUser = function(newUser,callback){
-    bcrypt.getSalt(10,(err,salt) =>{
-        bcrypt.hash(newUser.password,salt,(err,hash)=>{
-            if(err) throw err;
-            newUser.password = hash;
-            newUser.save(callback);
-        });
-    });
-}
-
-module.exports.comparePassword = function(candidatePassword,hash,callback){
-    bcrypt.compare(candidatePassword,hash,(err,isMatch)=>{
+  });
+  
+  const User = module.exports = mongoose.model('User', UserSchema);
+  
+  module.exports.getUserById = function(id, callback){
+    User.findById(id, callback);
+  }
+  
+  module.exports.getUserByUsername = function(username, callback){
+    const query = {username: username}
+    User.findOne(query, callback);
+  }
+  
+  module.exports.addUser = function(newUser, callback){
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
         if(err) throw err;
-
-        callback(null,isMatch)
-    })
-}
+        newUser.password = hash;
+        newUser.save(callback);
+      });
+    });
+  }
+  
+  module.exports.comparePassword = function(candidatePassword, hash, callback){
+    bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
+      if(err) throw err;
+     
+      callback(null, isMatch);
+    });
+  }
+  
