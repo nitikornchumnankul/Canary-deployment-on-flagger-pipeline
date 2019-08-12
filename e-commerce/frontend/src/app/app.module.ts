1e7ef7c7eb2064f64fa23a52d3c3585a35878bc9
Nitikorn Chumnankul <nitikornchumnankul@gmail.com>
2019-08-12 14:17:37 +0700

    register component active

--- e-commerce/frontend/src/app/app.module.ts
+++ e-commerce/frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
