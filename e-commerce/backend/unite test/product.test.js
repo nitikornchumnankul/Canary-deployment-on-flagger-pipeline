1c31130f6fd2a34413030f0215f46bba7f8fc0c5
Nitikorn Chumnankul <nitikornchumnankul@gmail.com>
2019-08-15 04:47:32 +0700

    check trigger before merge to master

--- e-commerce/backend/unite test/product.test.js
+++ e-commerce/backend/unite test/product.test.js
@@ -10,16 +10,16 @@ test("name should not have special charactor",() =>{
     expect(newProduct.name).not.toMatch(/!@#$%^&*(),.?":{}|<>/);
 });
 
-test('Catag should not have special charactor', () => {
+test('img should not have special charactor', () => {
   expect(newProduct.img).not.toMatch(/!@#$%^&*(),.?":{}|<>/);
 });
 
 
-test('Catag should not have special charactor ', () => {
+test('description should not have special charactor ', () => {
   expect(newProduct.description).not.toMatch(/!@#$%^&*(),.?":{}|<>/);
 });
 
-test('Catag should not have special charactor', () => {
+test('Catag should not have special charactor hello', () => {
   expect(newProduct.Catag).not.toMatch(/!@#$%^&*(),.?":{}|<>/);
 });
 
