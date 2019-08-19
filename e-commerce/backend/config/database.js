abb6f53d19244783c6b61425fc64d9e96eecebc3
nitikornchumnankul <nitikornchumnankul@gmail.com>
2019-08-20 00:17:47 +0700

    validate model

--- e-commerce/backend/config/database.js
+++ e-commerce/backend/config/database.js
@@ -1,5 +1,5 @@
 module.exports ={
-    // database:'mongodb://localhost:27017/souvenir',
-    database:'mongodb://192.168.100:27017/souvenir',
+    database:'mongodb://localhost:27017/souvenir',
+    // database:'mongodb://192.168.100:27017/souvenir',
     secret: 'yoursecret'
 }
