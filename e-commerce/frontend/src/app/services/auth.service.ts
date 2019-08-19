322eb51c5780d664b2c86d2d012eaf253d1ea771
nitikornchumnankul <nitikornchumnankul@gmail.com>
2019-08-20 00:17:20 +0700

    validate model

--- e-commerce/frontend/src/app/services/auth.service.ts
+++ e-commerce/frontend/src/app/services/auth.service.ts
@@ -18,20 +18,20 @@ export class AuthService {
   registerUser(user) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    // return this.http.post('http://localhost:8080/users/register', user, { headers: headers })
-    //   .map(res => res.json());
+    return this.http.post('http://localhost:8080/users/register', user, { headers: headers })
+      .map(res => res.json());
 
-    return this.http.post('http://192.168.99.100:8080/users/register', user, { headers: headers })
-    .map(res => res.json());
+    // return this.http.post('http://192.168.99.100:8080/users/register', user, { headers: headers })
+    // .map(res => res.json());
   };
   authenticateUser(user) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    // return this.http.post('http://localhost:8080/users/authenticate', user, { headers: headers })
-    //   .map(res => res.json());
-
-      return this.http.post('http://192.168.99.100:8080/users/authenticate', user, { headers: headers })
+    return this.http.post('http://localhost:8080/users/authenticate', user, { headers: headers })
       .map(res => res.json());
+
+      // return this.http.post('http://192.168.99.100:8080/users/authenticate', user, { headers: headers })
+      // .map(res => res.json());
   }
   loadToken(){
     const token = localStorage.getItem('id_token');
@@ -57,10 +57,11 @@ export class AuthService {
 
     headers.append('Authorization', this.authToken);
     headers.append('Content-Type', 'application/json');
-    // return this.http.post('http://localhost:8080/users/addproduct', product, { headers: headers })
-    //   .map(res => res.json());
-      return this.http.post('http://192.168.99.100:8080/users/addproduct', product, { headers: headers })
+    return this.http.post('http://localhost:8080/users/addproduct', product, { headers: headers })
       .map(res => res.json());
+
+      // return this.http.post('http://192.168.99.100:8080/users/addproduct', product, { headers: headers })
+      // .map(res => res.json());
   }
 
   userRole() {
