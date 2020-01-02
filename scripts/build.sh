7aa87724e286f1953a4b67844b5c76edd2be820b
nitikornchumnankul <nitikornchumnankul@gmail.com>
2020-01-03 02:41:00 +0700

    add path

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 
 set -env
 
-SCRIPT_DIR=$(dirname "$0")
+SCRIPT_DIR="./e-commerce/frontend"
 
 if [[ -z "$GROUP"]] ; then
     echo "Cannot find GROUP env var"
@@ -23,6 +23,6 @@ fi
 CODE_DIR=$(cd $SCRIPT_DIR/..; pwd)
 echo $CODE_DIR
 
-REPO=$(GROUP)/$(basename E-COMMERCE-AND-CANARY-DEPLOYMENT);
+REPO=$(GROUP)/$(basename frontend);
 
 $DOCKER_CMD build -t ${REPO}:${COMMIT} .
