89305bdd14b8b32b7577f03d22d574ff43570281
Nitikorn Chumnankul <nitikornchumnankul@gmail.com>
2020-01-07 20:26:24 +0700

    frontend to backend in build.sh file

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 
 set -env
 
-SCRIPT_DIR="./Application/frontend"
+SCRIPT_DIR="./Application/backend"
 
 if [[ -z "$GROUP"]] ; then
     echo "Cannot find GROUP env var"
@@ -23,6 +23,6 @@ fi
 CODE_DIR=$(cd $SCRIPT_DIR/..; pwd)
 echo $CODE_DIR
 
-REPO=$(GROUP)/$(basename frontend);
+REPO=$(GROUP)/$(basename backend);
 
 $DOCKER_CMD build -t ${REPO}:${COMMIT} .
