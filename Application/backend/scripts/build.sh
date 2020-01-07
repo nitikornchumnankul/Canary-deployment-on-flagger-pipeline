cece082f938243a1deab1f6b91b5d49366720809
Nitikorn Chumnankul <nitikornchumnankul@gmail.com>
2020-01-07 20:50:03 +0700

    add folder script file in backend folder

--- Application/backend/scripts/build.sh
+++ Application/backend/scripts/build.sh
@@ -1,28 +1,28 @@
-#!/usr/bin/env bash
+#! /usr/bin/env bash
 
-set -ev
+set -env
 
-SCRIPT_DIR=$(dirname "$0")
+SCRIPT_DIR="./Application/backend"
 
-if [[ -z "$GROUP" ]] ; then
-  echo "Cannot find GROUP env var"
-  exit 1
+if [[ -z "$GROUP"]] ; then
+    echo "Cannot find GROUP env var"
+    exit 1
+fi 
+
+if [[ - "$COMMIT"]] ; then
+    echo "Cannot find COMMIT env var"
+    exit 1
 fi
 
-if [[ -z "$COMMIT" ]] ; then
-  echo "Cannot find COMMIT env var"
-  exit 1
-fi
-
-if [[ "$(uname)" == "Darwin" ]]; then
-  DOCKER_CMD=docker
-else
-  DOCKER_CMD="sudo docker"
+if [["$(uname)" == "nitikornchumnankul"]] ; then
+    DOCKER_CMD=docker 
+ else
+    DOCKER_CMD="sudo docker"
 fi
 
 CODE_DIR=$(cd $SCRIPT_DIR/..; pwd)
 echo $CODE_DIR
 
-REPO=${GROUP}/$(basename front-end);
+REPO=$(GROUP)/$(basename backend);
 
 $DOCKER_CMD build -t ${REPO}:${COMMIT} .
