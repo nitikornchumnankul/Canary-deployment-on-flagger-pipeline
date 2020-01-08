19da3a73ab7a81d8693132e0f18ba0958fc8204f
Nitikorn <nitikornchumnankul@gmail.com>
2020-01-08 23:25:38 +0700

    add eacho

--- Application/backend/scripts/push.sh
+++ Application/backend/scripts/push.sh
@@ -9,7 +9,9 @@ if [[ -z $COMMIT ]] ; then
     exit 1
 fi
 echo $COMMIT
-
+echo ${TRAVIS_TAG}
+echo $1
+echo $TAG
 push(){
     DOCKER_PUSH=1;
     while [ $DOCKER_PUSH -gt 0 ] ; do 
@@ -36,8 +38,7 @@ tag_and_push_all(){
 }
 #Push snapshot when in master
 if [ $TRAVIS_BRANCH == "master" ] && [ $TRAVIS_PULL_REQUEST == "false" ]; then
-    echo ${TRAVIS_TAG}
-    echo $1
+    
     tag_and_push_all ${TRAVIS_TAG}
     tag_and_push_all latest
 fi;
